8b303e2f55ba57c61e890263d49d92cd3f6c900e
idea house <ideahouse.tech@gmail.com>
2026-05-08 22:16:24 +0700

    Remove duplicate plugin declaration to resolve Kotlin version conflict

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -23,7 +23,4 @@ tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
 
-plugins {
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-}
+
